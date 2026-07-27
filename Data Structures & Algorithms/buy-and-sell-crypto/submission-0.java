3e939b2343c0864bc0bdc2a8a7e380d6f4a37948
Vibhrav Jha <133233332+V1R4V@users.noreply.github.com>
2026-07-27 18:52:47 -0400

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxprofit =0;
+        int profit;
+        int minprice= Integer.MAX_VALUE;
+
+        for (int i = 0; i<(prices.length);i++){
+          if (prices[i]< minprice){
+            minprice = prices[i];
+          }
+          else{
+            profit = prices[i]-minprice;
+            maxprofit = Math.max(maxprofit,profit);
+          }
+        }
+    return maxprofit;
+
+
+    }
+}
